1d87eda38ac88c1faecd1c9ed2552777af345443
PrashanthSubramani <prashanthsubramani1699@gmail.com>
2024-10-20 17:53:36 +0530

    Minor changes in budget Tracker v1.0.2

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Navbar from '@/components/Navbar'
+import React, { ReactNode } from 'react'
 
-function layout() {
+function layout({children}:{children: ReactNode}) {
   return (
-    <div>layout</div>
+    <div className='relative flex h-screen w-full flex-col'>
+      <Navbar/>
+      <div className="w-full">
+        {children}
+      </div>
+    </div>
   )
 }
 
